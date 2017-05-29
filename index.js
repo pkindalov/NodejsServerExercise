032cf6e0f6d57137772a844dff51a529e1a8e90a
pkindalov <panayotkindalov@gmail.com>
2017-05-29 16:53:27 +0300

    Init the project. Add skeleton of project

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const url = require('url');
+const port = 1204;
+
+const handlers = require('./handlers');
+
+
+http
+    .createServer((req, res) => {
+        req.path = url.parse(req.url).pathname;
+        
+        for(let handler of handlers){
+            let next = handler(req, res);
+            if(!next){
+                break;
+            }
+        }
+
+    
+    })
+        .listen(port);
+
+console.log(`Server started at ${port} port`);
