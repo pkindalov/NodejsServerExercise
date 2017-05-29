de9a65be0c01d40417ef13349682ab27e3081216
pkindalov <panayotkindalov@gmail.com>
2017-05-29 22:19:32 +0300

    Update home-page.js

--- handlers/home-page.js
+++ handlers/home-page.js
@@ -1,14 +1,33 @@
 const fs = require('fs');
+const database = require('../database/database');
 
 module.exports = (req, res) => {
 
     if(req.path === '/'){
 
-       fs.readFile('./index.html', (err, data) => {
+       fs.readFile('./index.html', 'utf8', (err, data) => {
                 if(err){
                     console.log(err);
                     return;
                 }
+
+                let images = database.getAll();
+
+                let result = ' ';
+                result += '<ul>';
+
+                for (let image of images){
+                    result += `
+                    <li>
+                        <a href="${image.url}">${image.name}</a>
+                    </li>
+                    
+                    `;
+                }
+
+                result += '</ul>';
+
+                data = data.replace('{{content}}', result);
 
                 res.writeHead(200, {
                     'Content-type': 'text/html'
