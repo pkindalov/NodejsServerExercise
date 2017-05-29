8022c4fcc6dd96c5ad21359ba9092b4c1cfb7351
pkindalov <panayotkindalov@gmail.com>
2017-05-29 17:37:10 +0300

    Update image-upload-handler.js file in handlers directory

--- handlers/image-upload-handler.js
+++ handlers/image-upload-handler.js
@@ -1,4 +1,4 @@
-
+const fs = require('fs');
 
 module.exports = (req, res) => {
     if(req.path !== '/images/upload'){
@@ -7,8 +7,22 @@ module.exports = (req, res) => {
 
     if(req.method === 'GET'){
 
+        fs.readFile('./views/image-upload-form.html', (err, data) => {
+            if(err){
+                console.log(err);
+                return;
+            }
+
+            res.writeHead(200, {
+                'Content-type': 'text/html'
+            });
+
+            res.write(data);
+            res.end();
+        });
+
     }else if(req.method === 'POST'){
-        
+
     }
 
 };
