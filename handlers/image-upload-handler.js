24af2174c545de58ae58b25540a9503a73fc0ad0
pkindalov <panayotkindalov@gmail.com>
2017-05-29 21:44:01 +0300

    Updated image-upload-handler.js Added back button on image-upload-error.html

--- handlers/image-upload-handler.js
+++ handlers/image-upload-handler.js
@@ -33,13 +33,36 @@ module.exports = (req, res) => {
         req.on('end', () => {
             let imageData = query.parse(result);
 
-            database.save(imageData);
 
-            res.writeHead(302, {
-                Location: '/'
-            });
+           let imageName = imageData['image-name'];
+           let imageURL = imageData['image-url'];
 
-            res.end();
+           if(!imageName || !imageURL){
+               fs.readFile('./views/image-upload-error.html', (err, data) => {
+                    if(err){
+                        console.log(err);
+                        return;
+                    }
+
+                    res.writeHead(200, {
+                        'Content-type': 'text/html'
+                    });
+
+                    res.write(data);
+                    res.end();
+                  
+               });
+           } else {
+
+                     database.save(imageData);
+
+                     res.writeHead(302, {
+                         Location: '/'
+                    });
+
+                    res.end();
+           }
+          
 
         });
     }
